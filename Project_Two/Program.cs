667c0474c4190f9ccbe657b5ca0e6d589e069760
Abraham Huerta <Abrhuerta@gmail.com>
2019-02-11 17:35:22 -0600

    added queries class

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -11,7 +11,7 @@ namespace Project_Two
             /**Your application should allow the end user to pass end a file path for output 
             * or guide them through generating the file.
             **/
-            
+
             List<string> rows = new List<string>();
             using (StreamReader reader = new StreamReader(@"..\..\..\Super_Bowl_Project.csv")) {
                 while (!reader.EndOfStream)
@@ -25,68 +25,78 @@ namespace Project_Two
             {
                 superbowls.Add(new Superbowl(row.Split(',')));
             }
+            SuperbowlQuerries queries = new SuperbowlQuerries(superbowls);
             DisplayPrompt();
             Console.ReadKey();
-            
 
-        }
 
-        private static void DisplayPrompt()
-        {
-            Console.SetWindowSize(70, 10);
-            Console.SetCursorPosition(10, 2);
-            Console.WriteLine("Welcome to the superbowl analysis file displayer");
-            Console.SetCursorPosition(5, 3);
-            Console.WriteLine("Would you like the information in a text file or HTML file?");
-            interact();
-        }
 
-        private static void interact()
-        {
-            bool highlighted = false;
-            ConsoleKey userKey = new ConsoleKey();
-            while (userKey != ConsoleKey.Enter)
+
+            void DisplayPrompt()
             {
-                DisplayOptions(highlighted);
-                userKey = Console.ReadKey().Key;
-                switch (userKey)
+                Console.SetWindowSize(70, 10);
+                Console.SetCursorPosition(10, 2);
+                Console.WriteLine("Welcome to the superbowl analysis file displayer");
+                Console.SetCursorPosition(5, 3);
+                Console.WriteLine("Would you like the information in a text file or HTML file?");
+                interact();
+            }
+
+            void interact()
+            {
+                bool highlighted = false;
+                ConsoleKey userKey = new ConsoleKey();
+                while (userKey != ConsoleKey.Enter)
                 {
-                    case ConsoleKey.A:
-                        highlighted = false;
-                        break;
-                    case ConsoleKey.D:
-                        highlighted = true;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        highlighted = true;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        highlighted = false;
-                        break;
-                    default:
-                        break;
+                    DisplayOptions(highlighted);
+                    userKey = Console.ReadKey(true).Key;
+                    switch (userKey)
+                    {
+                        case ConsoleKey.A:
+                            highlighted = false;
+                            break;
+                        case ConsoleKey.D:
+                            highlighted = true;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            highlighted = true;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            highlighted = false;
+                            break;
+                        default:
+                            break;
+                    }
+                    
                 }
-
+                if (highlighted)
+                {
+                    queries.GenerateHTMLFile();
+                }
+                else
+                {
+                    queries.GenerateTextFile();
+                }
             }
-        }
 
-        private static void DisplayOptions(bool highlighted)
-        {
-            Console.CursorVisible = false;
-            Console.SetCursorPosition(7, 5);
-            if (!highlighted)
+            void DisplayOptions(bool highlighted)
             {
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.Write("Text File".PadLeft(20));
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("HTML File".PadLeft(20));
-            }
-            else
-            {
-                Console.Write("Text File".PadLeft(20));
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.Write("HTML File".PadLeft(20));
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(7, 5);
+                if (!highlighted)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write("Text File".PadLeft(20));
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("HTML File".PadLeft(20));
+                }
+                else
+                {
+                    Console.Write("Text File".PadLeft(20));
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write("HTML File".PadLeft(20));
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
             }
         }
     }
---
+++ Project_Two/SuperbowlQuerries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_Two
+{
+    class SuperbowlQuerries
+    {
+        IEnumerable<Team> winners;
+        public SuperbowlQuerries(List<Superbowl> bowls)
+        {
+            this.winners =
+                from bowl in bowls
+                from team in bowl.Teams
+                where bowl.Teams[0].IsWinner
+                select team;
+
+        }
+
+        public void GenerateTextFile()
+        {
+            foreach(Team team in this.winners)
+            {
+                Console.WriteLine(team.TeamName + team.Coach + team.Qb); 
+            }
+        }
+        public void GenerateHTMLFile()
+        {
+            Console.WriteLine("\nNot available yet");
+        }
+    }
+}
