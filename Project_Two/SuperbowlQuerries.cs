532ec233001e2a40c0e6f0c7eff0833c88feaa58
Abraham <Abrhuerta@gmail.com>
2019-02-12 13:12:35 -0600

    added the ability to create txt files

--- Project_Two/SuperbowlQuerries.cs
+++ Project_Two/SuperbowlQuerries.cs
@@ -2,27 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Project_Two
 {
     class SuperbowlQuerries
     {
-        IEnumerable<Team> winners;
+        IEnumerable<Superbowl> winners;
+        IEnumerable<Superbowl> mostAttended;
         public SuperbowlQuerries(List<Superbowl> bowls)
         {
             this.winners =
                 from bowl in bowls
                 from team in bowl.Teams
-                where bowl.Teams[0].IsWinner
-                select team;
+                where team.IsWinner
+                select bowl;
+            this.mostAttended =
+                from bowl in bowls
+                where bowl.Attendance > bowls[bowls.IndexOf(bowl) - 1].Attendance
+                select bowl;
 
         }
 
         public void GenerateTextFile()
         {
-            foreach(Team team in this.winners)
-            {
-                Console.WriteLine(team.TeamName + team.Coach + team.Qb); 
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = "Superbowl";
+            using (StreamWriter writer = File.CreateText(path + "\\" + fileName + ".txt")) {
+                foreach (Superbowl bowl in this.winners)
+                {
+                    writer.WriteLine(bowl.Year.PadRight(20) + bowl.SuperbowlTitle.PadRight(20) + bowl.Teams[0].TeamName.PadRight(20) + bowl.Teams[1].TeamName.PadRight(20));
+                }
             }
         }
         public void GenerateHTMLFile()
